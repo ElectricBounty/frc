58871856aba12d84c7919d1b62f5914d996b2f7a
DustyFrog <halsteadb3211@masseyhigh.school.nz>
2026-03-17 10:53:07 +1300

    fixed stuff

--- c_01_product_details.py
+++ c_01_product_details.py
@@ -90,7 +90,12 @@ def get_expenses(exp_type, amount):
     })
 
     # calc row cost
-    expense_frame["Cost"] = expense_frame["Amount"] * expense_frame["$ / Item"]
+    # IF fixed variables we just cost = $/item
+    if exp_type == "variable":
+        expense_frame["Cost"] = expense_frame["Amount"] * expense_frame["$ / Item"]
+    else:
+        expense_frame["Cost"] = expense_frame["$ / Item"]
+
 
     # return all items and subtotal
     subtotal = expense_frame["Cost"].sum()
@@ -110,8 +115,22 @@ while True:
     print(f"You are making {product_amount} {product_name}s")
     print()
 
+    # get variable expenses
+    print("Variable Expenses")
     variable_expenses = get_expenses("variable", product_amount)
     variable_df = variable_expenses[0]
     variable_subtotal = variable_expenses[1]
+
+    # get fixed expenses
+    print("Fixed Expenses")
+    fixed_expenses = get_expenses("fixed", product_amount)
+    fixed_df = fixed_expenses[0]
+    fixed_subtotal = fixed_expenses[1]
+
+    # print everything
     print(variable_df.to_string(index=False))
+    print(variable_subtotal)
+
+    print(fixed_df.to_string(index=False))
+    print(fixed_subtotal)
 
